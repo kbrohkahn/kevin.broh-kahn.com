a7ea328394a0e3eb545c8ef334b8990e11f4f301
Kevin Broh-Kahn <kbrohkahn@yahoo.com>
2016-01-13 18:18:07 -0500

    Add icon to Ratfink page and reorder header. Change top link on Ratfink page to text.

--- apps/ratfink.py
+++ apps/ratfink.py
@@ -5,12 +5,14 @@ with open("../templates/navbar.html", "r") as navbar:
 	print navbar.read()
 
 print("""
-<h1>Ratfink</h1>
-<div class="subheader">A combination of poker and bridge that's easy to learn and fun to play</div>
-<div class="badge-container">
-	<a target="blank" href="https://play.google.com/store/apps/details?id=com.play2think.ratfink">
-		<img alt="Get it on Google Play" src="/assets/img/en_generic_rgb_wo_45.png" />
-	</a>
+<div class="row">
+	<div class="col-xs-6 col-xs-offset-3 col-sm-offset-0 col-sm-3 col-md-2 col-lg-1">
+		<img class="img-responsive" alt="Ratfink Icon" src="/assets/img/ratfink/ic_launcher.png">
+	</div>
+	<div class="col-xs-12 col-sm-9 col-md-10 col-lg-11">
+		<h1>Ratfink <a target="blank" href="https://play.google.com/store/apps/details?id=com.play2think.ratfink"><small>Google Play</small></a></h1>
+		<div class="subheader">A combination of poker and bridge that's easy to learn and fun to play</div>
+	</div>
 </div>
 <h2>Description</h2>
 <p class="indented">Ratfink is a trick taking card game and a fun and easy way learn the basics of the bridge card game!</p>
