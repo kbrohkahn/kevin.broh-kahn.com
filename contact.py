00bb074cfa60689b921ae394123e54e545998cb5
Kevin Broh-Kahn <kbrohkahn@yahoo.com>
2016-01-26 12:52:40 -0500

    Add Stack Overflow profile.

--- contact.py
+++ contact.py
@@ -14,6 +14,7 @@ print("""
 			<h4 class="center">Get in Touch</h4>
 			<ul class="social-links">
 				<li><a title="View my LinkedIn Profile" href="https://www.linkedin.com/in/kbrohkahn" target="blank"><img src="/assets/img/social/LinkedIn-Icon.png"></a></li>
+				<li><a title="View my Stack Overflow Profile" href="http://stackoverflow.com/cv/kbrohkahn" target="blank"><img src="/assets/img/social/Stack-Overflow-Icon.png"></a></li>
 				<li><a title="View my GitHub Profile" href="https://github.com/kbrohkahn" target="blank"><img src="/assets/img/social/GitHub-Icon.png"></a></li>
 				<li><a title="View my apps on the Google Play Store" href="https://play.google.com/store/apps/dev?id=4654453048954028942" target="blank"><img src="/assets/img/social/Google-Play-Store-Icon.png"></a></li>
 				<li><a title="View my apps on the App Store" href="https://itunes.apple.com/us/developer/kevin-broh-kahn/id1047274120" target="blank"><img src="/assets/img/social/App-Store-Icon.png"></a></li>
