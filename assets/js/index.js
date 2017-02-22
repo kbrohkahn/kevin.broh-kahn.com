b606cc6e7f1e892ac3017685e17f93c543a1e86a
Kevin Broh-Kahn <kevin@broh-kahn.com>
2017-02-22 17:15:14 -0500

    Improve home page by hiding labels on small screens.

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,5 +10,5 @@ $(document).ready(function() {
 			opacity: 0
 		}, 250);
 	});
-	
+
 });
--- index.py
+++ index.py
@@ -5,41 +5,8 @@ with open("templates/header.html", "r") as header:
 with open("templates/navbar.html", "r") as navbar:
 	print navbar.read()
 
-print('<link href="/assets/css/circuit-background.css" rel="stylesheet">')
-print('<link href="/assets/css/index.css" rel="stylesheet">')
-
-print("""
-<div id="circuit-background">
-	<span class="home-span left-triangle-span"></span>
-	<span class="navigation-links">
-		<a href="/apps"><h4>Apps</h4></a>
-		<ul>
-			<li><a href="/apps/ratfink"><img alt="Ratfink icon" class="small-icon" src="/assets/img/ratfink/icon.png"><span>Ratfink</span></a></li>
-			<li><a href="/apps/music_home"><img alt="Music Home icon" class="small-icon" src="/assets/img/music_home/icon.png"><span>Music Home</span></a></li>
-			<li><a href="/apps/charging_wear"><img alt="Charging Wear icon" class="small-icon" src="/assets/img/charging_wear/icon.png"><span>Charging Wear</span></a></li>
-			<li><a href="/apps/omega"><img alt="Omega icon" class="small-icon" src="/assets/img/omega/icon.png"><span>Omega Watch Face</span></a></li>
-			<li><a href="/apps/prezcon"><img alt="Prezcon icon" class="small-icon" src="/assets/img/prezcon/icon.png"><span>Prezcon</span></a></li>
-			<li><a href="/apps/wbc"><img alt="WBC icon" class="small-icon" src="/assets/img/wbc/icon.png"><span>WBC</span></a></li>
-		</ul>
-	</span>
-	<span class="home-span right-triangle-span"></span>
-	<span class="navigation-links">
-		<a href="/projects"><h4>Projects</h4></a>
-		<ul>
-			<li><a href="/projects/view-recipes"><img alt="View Recipes icon" class="small-icon" src="/assets/img/view-recipes/icon.png"><span>View Recipes</span></a></li>
-			<li><a href="/projects/website"><img alt="This website icon" class="small-icon" src="/assets/img/kevin.broh-kahn.com/icon.png"><span>This website</span></a></li>
-			<li><a href="/projects/sixsided"><img alt="Six Sided icon" class="small-icon" src="/assets/img/sixsided.com/icon.png"><span>Six Sided Simulations</span></a></li>
-		</ul>
-	</span>
-	<a href="/contact" id="headshot">
-		<img class="img-circle" alt="My photo" src="/assets/img/headshot1.png">
-		<div>Get in touch</div>
-	</a>
-</div>
-""")
-
-print('<script src="/assets/js/circuit-background-script.js" type="text/javascript"></script>')
-print('<script src="/assets/js/index.js" type="text/javascript"></script>')
+with open("index_contents.html", "r") as page_html:
+	print page_html.read()
 
 with open("templates/footer.html", "r") as footer:
 	print footer.read()
