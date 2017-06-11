913d25b048797b32cc0cdd575b6d44db868b9674
Kevin Broh-Kahn <kevin@broh-kahn.com>
2017-06-11 13:48:43 -0400

    Initial redesign of index.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-	$("#headshot").mouseenter(function () {
-		$("#headshot div").animate({
-			opacity: 0.75
-		}, 250);
-	});
 
-	$("#headshot").mouseleave(function () {
-		$("#headshot div").animate({
-			opacity: 0
-		}, 250);
+	// $("#headshot").mouseenter(function () {
+	// 	$("#headshot div").animate({
+	// 		opacity: 0.75
+	// 	}, 250);
+	// });
+
+	// $("#headshot").mouseleave(function () {
+	// 	$("#headshot div").animate({
+	// 		opacity: 0
+	// 	}, 250);
+	// });
+
+
+	$(".section-link").click(function(e) {
+		var elementId = e.target;
+
+		$("html, body").animate({
+			scrollTop: $(elementId).offset().scrollTop;
+		}, 500);
+		// return false;
 	});
 
 });
---
+++ index_new.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+with open("templates/header.html", "r") as header:
+	print header.read()
+
+with open("index_contents_new.html", "r") as page_html:
+	print page_html.read()
+
+with open("templates/footer.html", "r") as footer:
+	print footer.read()
